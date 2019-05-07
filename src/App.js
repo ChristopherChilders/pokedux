da027a5eded9fb16f5cc58eae5d312ff33dcb2e3
Christopher Childers <christopher.childers13@yahoo.com>
2019-05-07 13:47:01 -0400

    uses smart version to display pokelist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Import the new smart component from containers
+import PokeList from './containers/PokeListContainer';
+// Testing the dummy component
+// import PokeList from './components/PokeList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PokeThings</h1>
+      <PokeList />
+      {/* <PokeList /> */}
     </div>
   );
 }
