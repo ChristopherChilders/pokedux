a1245931ee07a4ce6ea754f5755fdc26ab8ae0ca
Christopher Childers <christopher.childers13@yahoo.com>
2019-05-07 09:50:09 -0400

    adds skeleton to pokedux app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+////////////////////////////////////////////////////////////////////
+// State
+import initialState from './base.json';
+console.log(initialState);  
+
+// The state is an object with a cards property which is and array of objects
+// { cards: [{}, {}, {}] } 
+
+/////////////////////////////////////////////////////////////////////
+// ACTIONS + ACTION CREATORS
+const ACTION_CATCH = 'catch';
+
+function catchCard (id) {
+    return {
+        type: ACTION_CATCH,
+        payload: {
+            id,
+        }
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// REDUCER
+function cards(state=initialState, action={type: ''}){
+    switch(action.type){
+        case ACTION_CATCH:
+            // find the card, set it to "caught"
+        break;
+        default:
+            return state;
+        break;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+// STORE    
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
